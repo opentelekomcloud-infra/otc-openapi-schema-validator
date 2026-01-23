63d0b22db22eb3f2e3ca41cb26318c4a2d9eb1b0
anton-sidelnikov <a.sidelnikov@t-systems.com>
2026-01-23 12:30:32 +0100

    [Fix] unused import

--- src/functions/checkTimeZoneFormat.ts
+++ src/functions/checkTimeZoneFormat.ts
@@ -1,5 +1,4 @@
 import { Diagnostic } from "@codemirror/lint";
-import { mapSeverity } from "@/utils/mapSeverity";
 
 export function checkTimeZoneFormat(spec: any, content: string, rule: any): Diagnostic[] {
     const diagnostics: Diagnostic[] = [];
